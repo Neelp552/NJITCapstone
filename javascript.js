e97a64310948073701b6409739e0a840f4072311
neel <np552@njit.edu>
2020-10-27 20:01:30 -0400

    Init Commit
    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+{
+    copy: 'Copy',
+        copySuccess: {
+    1: "Copied one row to clipboard",
+        _: "Copied %d rows to clipboard"
+},
+    copyTitle: 'Copy to clipboard',
+        copyKeys: 'Press <i>ctrl</i> or <i>\u2318</i> + <i>C</i> to copy the table data<br>to your system clipboard.<br><br>To cancel, click this message or press escape.'
+}
